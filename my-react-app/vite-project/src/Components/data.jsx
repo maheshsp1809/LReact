bf37726144a48f3f39ab81b5b5dacb5c66dc954c
maheshsp <maheshsp1809@gmail.com>
2023-09-13 23:05:03 +0530

    deleted

--- my-react-app/vite-project/src/Components/data.jsx
+++
@@ -1,5 +0,0 @@
-const books=[
-    { id: 1, title: 'To Kill a Mockingbird', author: 'Harper Lee' },
-    { id: 2, title: '1984', author: 'George Orwell' },
-    { id: 3, title: 'The Great Gatsby', author: 'F. Scott Fitzgerald' },
-  ]
